38c27bc3a94a8609a52e02f4700423d00405dae8
loperret2 <loperret2@3fcf42c2-ae39-4f23-8ac7-369815a4a4d5>
2021-04-08 08:03:47 +0000

    Création fenêtre conexxion & profil

---
+++ Code/ProjetManga/ProjetManga/Connection_Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetManga
+{
+    /// <summary>
+    /// Logique d'interaction pour Connection_Window.xaml
+    /// </summary>
+    public partial class Connection_Window : Window
+    {
+        public Connection_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code/ProjetManga/ProjetManga/Profil_Window.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetManga
+{
+    /// <summary>
+    /// Logique d'interaction pour Profil_Window.xaml
+    /// </summary>
+    public partial class Profil_Window : Window
+    {
+        public Profil_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
