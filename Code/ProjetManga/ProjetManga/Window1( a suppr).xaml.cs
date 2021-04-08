334bba9fbf218f980bf6ee7e4db7b4bfce6c8281
sidallet <sidallet@3fcf42c2-ae39-4f23-8ac7-369815a4a4d5>
2021-04-08 09:52:46 +0000

    Amelioration fenetre profil + essai de premiers design sur les fenetres + modif diagramme cas (voir sur les fichiers)

--- Code/ProjetManga/ProjetManga/Window1( a suppr).xaml.cs
+++ Code/ProjetManga/ProjetManga/Window1( a suppr).xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,24 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ProjetManga
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void settings_button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
